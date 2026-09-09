6e8b1623ec7d1586210d4dac870d059bb18bccf0
Lakshmi Sweatha Pasupuleti <88676293+LakshmiSweathaPasupuleti@users.noreply.github.com>
2026-09-09 08:51:28 +0000

    first deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("Hello world, are you there!")
+st.write("check the local host")
