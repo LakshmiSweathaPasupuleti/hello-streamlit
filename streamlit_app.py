f5cfc4ca1d83d28ba78b1c438a10f2c99633218d
Lakshmi Sweatha Pasupuleti <88676293+LakshmiSweathaPasupuleti@users.noreply.github.com>
2026-09-09 09:47:59 +0000

    Guess game deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,17 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.write("Hello world, are you there!")
-st.write("check the local host")
+st.header("Guessing game")
+
+st.write("These are most engaging movies ever made! I like all of them but can you guess my ultimate favorite of them?")
+
+movies = ["Interstellar", "The Dark Knight", "Forest Gump", "KillBill", "A Beautiful Mind"]
+
+guess = st.selectbox("Guess my favorite one:", movies)
+
+if st.button("Submit"):
+    if guess == "The Dark Knight":
+        st.success("Hurray! The Dark Knight is my favorite movie!")
+        st.balloons()
+    else:
+        st.error("Oops wrong guess! Try again")
